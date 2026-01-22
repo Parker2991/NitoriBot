f788ae5df758fd005001c4df8766222527ece07d
Parker2991 <parkerallengriffith04@gmail.com>
2026-01-22 09:03:52 -0600

    restart development on Night of Nights because the code was not satisfying me and was limiting

--- bot/selfcare/entity/GameMode.ts
+++
@@ -1,9 +0,0 @@
-export default function GameMode (bot: any, config: any) {
-  bot.on('packet.game_state_change', (packet: any) => {
-    if (packet.reason === "change_game_mode") {
-      bot.selfcare.gamemode = packet.gameMode
-    } if (packet.reason === "win_game") {
-      bot._client.write('client_command', { action: 0 })
-    }
-  })
-}
--- bot/util/EntityUUID.ts
+++
@@ -1,5 +0,0 @@
-function entityUUID (uuid: any) {
-  // taken from https://gist.github.com/storycraft/7813dd0186e85daa393e1df9cfa19f2a
-  return uuid.replace(/-/g, '').match(/.{8}/g).map((str: any) => Number.parseInt(str, 16)).map((num: any) => Number.parseInt(num & 0x80000000 ? num - 0xffffffff - 1 : num))
-}
-export = entityUUID
